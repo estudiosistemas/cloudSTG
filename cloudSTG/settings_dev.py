9f91ec888f2e36f569c4c04a90f402bc8c1b8229
mCosta7172 <estudiosistemas@gmail.com>
2020-03-28 19:03:31 -0300

    Se separaron los setting para dev o prod

---
+++ cloudSTG/settings_dev.py
@@ -0,0 +1,26 @@
+# Configuracion Local
+from .settings_base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
