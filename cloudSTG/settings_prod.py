acabf482d9217f373285b79eea0bb600d45a892c
mCosta7172 <estudiosistemas@gmail.com>
2020-03-29 17:16:52 -0300

    cambio a prod

--- cloudSTG/settings_prod.py
+++ cloudSTG/settings_prod.py
@@ -8,14 +8,21 @@ ALLOWED_HOSTS = ['172.105.148.177', '192.168.232.139']
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'cloudstg',
+#         'USER': 'mcosta',
+#         'PASSWORD': 'Ro190180',
+#         'PORT': 5432,
+#         'HOST': 'localhost'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cloudstg',
-        'USER': 'mcosta',
-        'PASSWORD': 'Ro190180',
-        'PORT': 5432,
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'cloudSTG.settings_dev')
+                          'cloudSTG.settings_prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
