627ac48de4722fb370a3a79cfa24eb92a5759d7e
mCosta7172 <estudiosistemas@gmail.com>
2020-03-29 16:34:01 -0300

    Configuracion archivo settings en producion

--- cloudSTG/settings_prod.py
+++ cloudSTG/settings_prod.py
@@ -3,15 +3,19 @@ from .settings_base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.105.148.177', '192.168.232.139']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cloudstg',
+        'USER': 'mcosta',
+        'PASSWORD': 'Ro190180',
+        'PORT': 5432,
+        'HOST': 'localhost'
     }
 }
 
@@ -20,6 +24,8 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = (BASE_DIR,  'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
