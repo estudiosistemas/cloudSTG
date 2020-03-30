4e13ac9d8c8b7b6ab61ec42d22acf2582df20053
mCosta7172 <estudiosistemas@gmail.com>
2020-03-30 08:30:07 -0300

    Cambio DB a Postgres

--- cloudSTG/settings_dev.py
+++ cloudSTG/settings_dev.py
@@ -8,13 +8,23 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cloudstg',
+        'USER': 'postgres',
+        'PASSWORD': 'adriel7172',
+        'PORT': 5432,
+        'HOST': 'localhost'
     }
 }
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
